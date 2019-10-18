ee9d1b3cfaa55c80096499869c8d4764a666a044
Enrico <e.ipodda@gmail.com>
2019-10-18 17:18:49 +0200

    update roles_template.json

--- test.py
+++ test.py
@@ -10,12 +10,12 @@ with open('data/raw_ico_json_2019-09-19/ICOBench_ongoing_2019-09-19.json', 'r') 
 with open('data/raw_ico_json_2019-09-19/ICOBench_upcoming_2019-09-19.json', 'r') as file:
     icos = icos + json.load(file)
 
-#populate_csv_files(icos, 'data/parsed') #when files need be created
+#populate_csv_files(icos, 'data/parsed/') #when files need be created
 
 
 path_to_rank_of_roles = "data/parsed/rank_of_roles.csv"
 path_to_template_file = "data/templates/roles_template.json"
-is_recursive = False # if True checks also substrings in the examples
+is_recursive = True # if True checks also substrings in the examples
 
 json_of_roles = get_json_of_roles(path_to_rank_of_roles, path_to_template_file, is_recursive)
 
--- utils/save.py
+++ utils/save.py
@@ -8,10 +8,10 @@ def populate_csv_files(icos, path_to_files):
     dict_of_roles = get_dict_of_roles(list_of_roles, set_of_roles)
     rank_of_roles = dict_to_rank(dict_of_roles)
 
-    write_list_to_file(path_to_files + "/list_of_roles.csv", list_of_roles)
-    write_list_to_file(path_to_files + "/set_of_roles.csv", set_of_roles)
-    write_dict_to_file(path_to_files + "/dict_of_roles.csv", dict_of_roles)
-    write_list_to_file(path_to_files + "/rank_of_roles.csv", rank_of_roles)
+    write_list_to_file(path_to_files + "list_of_roles.csv", list_of_roles)
+    write_list_to_file(path_to_files + "set_of_roles.csv", set_of_roles)
+    write_dict_to_file(path_to_files + "dict_of_roles.csv", dict_of_roles)
+    write_list_to_file(path_to_files + "rank_of_roles.csv", rank_of_roles)
 
 
 def write_list_to_file(path_to_file, list_of_lines):
