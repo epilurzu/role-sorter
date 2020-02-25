682c19d7f9d809bbbd2989ef0f0896cfb38d25a4
Enrico <e.ipodda@gmail.com>
2020-02-25 19:52:57 +0100

    minor refactor

--- utils.py
+++ utils.py
@@ -78,7 +78,7 @@ def progress(count, total):
     filled_len = int(round(bar_len * count / float(total)))
 
     percents = round(100.0 * count / float(total), 1)
-    bar = '=' * filled_len + '-' * (bar_len - filled_len)
+    bar = '=' * filled_len + ' ' * (bar_len - filled_len)
 
     sys.stdout.write('[%s] %s%s\r' % (bar, percents, '%'))
     sys.stdout.flush()
